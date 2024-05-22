105b8ad7601f646d24fc1a511c4000ba6f868cdb
frosther2021 <160144847+frosther2021@users.noreply.github.com>
2024-05-22 08:59:53 -0300

    Add files via upload

---
+++ styles/header.cs
@@ -0,0 +1,15 @@
+.cabeçalho_menu-hamburguer {
+    width: 24px;
+    height: 24px;
+    background-image: url("../img/Menu.svg");
+    display: inline-block;
+    background-repeat: no-repeat;
+    background-position: center;
+}
+
+.cabeçalho {
+    background-color: var(--branco);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
